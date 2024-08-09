57935152b931e4ce494b0eb696c10523c2a9a114
GeekySans <briggstaillefer10@gmail.com>
2024-08-08 17:32:32 -0700

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,12 +1,6 @@
-let save = 0;
-
 function back() {
   window.history.back();
 }
 function forward() {
   window.history.forward();
-}
-
-function bookmark1() {
-  localStorage.setItem("blog", "save");
 }
