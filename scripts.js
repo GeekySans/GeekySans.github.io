e5b51ef803e7a746e5a54d4456df49ed32ef1d67
GeekySans <briggstaillefer10@gmail.com>
2024-09-22 10:26:47 -0700

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -5,11 +5,7 @@ function back() {
 function forward() {
   window.history.forward();
 }
-
-let blogs =
-    "<h2>My-Blogs</h2>" + 
-    "<table><tr><td><h3>My Coding History</h3> <img src='something.gif'> <p>This is about my coding joruney</p> <p><a href='/historyofcoding'><button>View-Blog</button></a></p></td>" + 
-    "<td><h3>My OCD</h3> <img src='OCD.gif' height='50px' width='50px'> <p>Well it's about my OCD and this weird monster inside of my head that wants to destory everything.</p> <p><a href='/OCD'><button>View-Blog</button></a></p></td></tr><table>
+let blogs = "<h2>My-Blogs</h2>" + "<table><tr><td><h3>My Coding History</h3> <img src='something.gif'> <p>This is about my coding joruney</p> <p><a href='/historyofcoding'><button>View-Blog</button></a></p></td>" + "<td><h3>My OCD</h3> <img src='OCD.gif' height='50px' width='50px'> <p>Well it's about my OCD and this weird monster inside of my head that wants to destory everything.</p> <p><a href='/OCD'><button>View-Blog</button></a></p></td></tr><table>
 ;
 
 document.getElementById("blogs").innerHTML = blogs;
