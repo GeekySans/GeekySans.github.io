1f3b1fdf7ae0d602f88f66fb26aab9e88f657ce9
GeekySans <briggstaillefer10@gmail.com>
2024-09-22 19:54:21 -0700

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,3 +1,7 @@
+let blogs = "<h2>My-Blogs</h2>" + "<table><tr><td><h3>My Coding History</h3> <img src='something.gif'> <p>This is about my coding joruney</p> <p><a href='/historyofcoding'><button>View-Blog</button></a></p></td>" + "<td><h3>My OCD</h3> <img src='OCD.gif' height='50px' width='50px'> <p>Well it's about my OCD and this weird monster inside of my head that wants to destory everything.</p> <p><a href='/OCD'><button>View-Blog</button></a></p></td></tr><table>";
+
+document.getElementById("blogtable").innerHTML = blogs;
+
 function back() {
   window.history.back();
 }
@@ -5,6 +9,3 @@ function back() {
 function forward() {
   window.history.forward();
 }
-let blogs = "<h2>My-Blogs</h2>" + "<table><tr><td><h3>My Coding History</h3> <img src='something.gif'> <p>This is about my coding joruney</p> <p><a href='/historyofcoding'><button>View-Blog</button></a></p></td>" + "<td><h3>My OCD</h3> <img src='OCD.gif' height='50px' width='50px'> <p>Well it's about my OCD and this weird monster inside of my head that wants to destory everything.</p> <p><a href='/OCD'><button>View-Blog</button></a></p></td></tr><table>";
-
-document.getElementById("blogs").innerHTML = blogs;
