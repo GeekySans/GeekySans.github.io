c862b24b8724253a17b311a73f3756f2b1d5d1dc
GeekySans <briggstaillefer10@gmail.com>
2024-09-25 18:05:47 -0700

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,6 +1,7 @@
 let blogs = "<h2>My-Blogs</h2>" + "<table><tr><td><h3>My Coding History</h3> <img src='something.gif'> <p>This is about my coding joruney</p> <p><a href='/historyofcoding'><button>View-Blog</button></a></p></td>" + "<td><h3>My OCD</h3> <img src='OCD.gif' height='50px' width='50px'> <p>Well it's about my OCD and this weird monster inside of my head that wants to destory everything.</p> <p><a href='/OCD'><button>View-Blog</button></a></p></td></tr><table>";
 
-document.getElementsByClassName("blogtable").innerHTML = blogs;
+const blogtable = document.getElementsByClassName("blogtable");
+blogtable.innerHTML = blogs;
 
 function back() {
   window.history.back();
