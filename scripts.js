763e8298acd2791f2f44e98150c05889d70aff2b
GeekySans <briggstaillefer10@gmail.com>
2024-11-04 18:25:58 -0800

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -3,15 +3,22 @@ let blogs = "<h2>My-Blogs</h2>" + "<table><tr><td><h3>My Coding History</h3> <im
 const blogtable = document.getElementsByClassName("blogtable");
 blogtable[0].innerHTML = blogs;
 
-let posts = [];
+let posts = [
+"<h2>My-Blogs</h2>",
+"<table>",
+"<tr>",
+"<td><h3>My Coding History</h3> <img src='something.gif'> <p>This is about my coding joruney</p> <p><a href='/historyofcoding'><button>View-Blog</button></a></p></td>",
+"<td><h3>My OCD</h3> <img src='OCD.gif' height='50px' width='50px'> <p>Well it's about my OCD and this weird monster inside of my head that wants to destory everything.</p> <p><a href='/OCD'><button>View-Blog</button></a></p></td>"
+"</tr>"
+"<table>";
+];
 let data = "";
-posts.push("<h2>My Forum is open!</h2>");
-posts.push("<h2>My Forum is closed!</h2>");
-posts.push("<h2>My Forum is reopened!</h2>");
-for (let w = 0; w < 3; w++) {
+
+for (let w = 0; w < posts.length; w++) {
 data = data + posts[w];
-document.getElementById("post-table").innerHTML = data;
 }
+
+document.getElementById("post-table").innerHTML = data;
 
 function back() {
   window.history.back();
