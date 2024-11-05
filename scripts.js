cba952958d52556e9710ce7bda78a00a003180d3
GeekySans <briggstaillefer10@gmail.com>
2024-11-04 20:16:00 -0800

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,9 +1,4 @@
-let blogs = "<h2>My-Blogs</h2>" + "<table><tr><td><h3>My Coding History</h3> <img src='something.gif'> <p>This is about my coding joruney</p> <p><a href='/historyofcoding'><button>View-Blog</button></a></p></td>" + "<td><h3>My OCD</h3> <img src='OCD.gif' height='50px' width='50px'> <p>Well it's about my OCD and this weird monster inside of my head that wants to destory everything.</p> <p><a href='/OCD'><button>View-Blog</button></a></p></td></tr><table>";
-
-const blogtable = document.getElementsByClassName("blogtable");
-blogtable[0].innerHTML = blogs;
-
-let posts = [
+let blogs = [
 "<h2>My-Blogs</h2>",
 "<table>",
 "<tr>",
