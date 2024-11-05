5e79fa6c87c6f589f4d5e8535c7ce5a85fbfe4e0
GeekySans <briggstaillefer10@gmail.com>
2024-11-04 18:39:33 -0800

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -8,9 +8,9 @@ let posts = [
 "<table>",
 "<tr>",
 "<td><h3>My Coding History</h3> <img src='something.gif'> <p>This is about my coding joruney</p> <p><a href='/historyofcoding'><button>View-Blog</button></a></p></td>",
-"<td><h3>My OCD</h3> <img src='OCD.gif' height='50px' width='50px'> <p>Well it's about my OCD and this weird monster inside of my head that wants to destory everything.</p> <p><a href='/OCD'><button>View-Blog</button></a></p></td>"
-"</tr>"
-"<table>";
+"<td><h3>My OCD</h3> <img src='OCD.gif' height='50px' width='50px'> <p>Well it's about my OCD and this weird monster inside of my head that wants to destory everything.</p> <p><a href='/OCD'><button>View-Blog</button></a></p></td>",
+"</tr>",
+"<table>"
 ];
 let data = "";
 
