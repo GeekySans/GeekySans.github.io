e565e659122dafe0721bca7241cacbce7b9d1e44
GeekySans <briggstaillefer10@gmail.com>
2024-11-05 11:46:14 -0800

    Update grabblogs.js

--- grabblogs.js
+++ grabblogs.js
@@ -1,1 +1,24 @@
+let blogs;
 
+function loadblogs() {
+blogs = [
+"<h2>My-Blogs</h2>",
+"<table>",
+"<tr>",
+"<td><h3>My Coding History</h3> <img src='something.gif'> <p>This is about my coding joruney</p> <p><a href='/historyofcoding'><button>View-Blog</button></a></p></td>",
+"<td><h3>My OCD</h3> <img src='OCD.gif' height='50px' width='50px'> <p>Well it's about my OCD and this weird monster inside of my head that wants to destory everything.</p> <p><a href='/OCD'><button>View-Blog</button></a></p></td>",
+"</tr>",
+"<table>"
+];
+}
+
+function grabblogs() {
+let data = "";
+for (let w = 0; w < blogs.length; w++) {
+data = data + blogs[w];
+}
+document.getElementById("blogtable").innerHTML = data;
+}
+
+loadblogs();
+grabblogs();
