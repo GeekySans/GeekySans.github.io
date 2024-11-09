278b73dbd339bd1096a26bec6a821fbdc0a56e83
GeekySans <briggstaillefer10@gmail.com>
2024-11-08 16:58:48 -0800

    Create grabposts.js

---
+++ grabposts.js
@@ -0,0 +1,25 @@
+let posts;
+let data;
+
+function loadposts() {
+posts = [
+"<h2>My Posts</h2>",
+"<table>",
+"<tr>",
+"<td><p></p></td>",
+"<td></td>",
+"</tr>",
+"<table>"
+];
+}
+
+function grabposts() {
+data = "";
+for (let w = 0; w < posts.length; w++) {
+data = data + posts[w];
+}
+document.getElementById("blogtable").innerHTML = data;
+}
+
+loadposts();
+grabposts();
