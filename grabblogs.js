e12de74cb4cb9d94706d432c84b11ff6cf496d81
GeekySans <briggstaillefer10@gmail.com>
2024-11-09 12:26:49 -0800

    Update grabblogs.js

--- grabblogs.js
+++ grabblogs.js
@@ -7,7 +7,7 @@ blogs = [
 "<table>",
 "<tr>",
 "<td><h3>My Coding History</h3> <img src='something.gif'> <p>This is about my coding joruney</p> <p><a href='/historyofcoding'><button>View-Blog</button></a></p></td>",
-"<td><h3>My OCD</h3> <img src='OCD.gif' height='50px' width='50px'> <p>Well it's about my OCD and this weird monster inside of my head that wants to destory everything.</p> <p><a href='/OCD'><button>View-Blog</button></a></p></td>",
+"<td><h3>My OCD</h3> <img src='OCD.gif' height='50%' width='50%'> <p>Well it's about my OCD and this weird monster inside of my head that wants to destory everything.</p> <p><a href='/OCD'><button>View-Blog</button></a></p></td>",
 "</tr>",
 "<table>"
 ];
