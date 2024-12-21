fad76216418dda8abf3c21292bcd3c6e4d80c764
GeekySans <briggstaillefer10@gmail.com>
2024-12-20 18:27:38 -0800

    Update grabmenu.js

--- grabmenu.js
+++ grabmenu.js
@@ -1,9 +1,20 @@
 let menu;
 let data1;
+let greeting;
+
+if (hourNow > 18) {
+greeting = 'Good evening'
+} else if (hourNow > 12) {
+greeting = 'Good afternoon'
+} else if (hourNow > 0) {
+greeting = 'Good morning'
+} else {
+greeting = 'Welcome'
+}
 
 function loadmenu() {
 menu = [
-"<h1>GeekySans's Digital Playground</h1>",
+"<h1>GeekySans's Digital Playground</h1>", greeting,
 "<div> <a href='/home'><button class='home'>Home</button></a> <a href='https://spacehey.com/profile?id=2706928'><button class='spacehey'>Spacehey</button></a> <a href='https://nerdie-board.forumotion.com/'><button class='forum'>Forum</button></a> <a href='/blogs'><button class='blogs'>Blogs</button></a> <a href='/games'><button class='games'>Games</button></a> <a href='/posts'><button class='posts'>Posts</button></a> </div>",
 "<div> <button onclick='back()'>Back</button> <button onclick='forward()'>Forward</button> <a href='/search'><button type='submit' class='search'><img src='spyglass.png' width='20' height='20'></button></a> </div>",
 "<hr>"
