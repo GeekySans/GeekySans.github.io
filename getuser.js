7b98524f17c91374f4f55ffa945620b75a464725
GeekySans <briggstaillefer10@gmail.com>
2025-01-25 12:07:26 -0800

    Update and rename scripts.js to getuser.js

---
+++ getuser.js
@@ -0,0 +1,67 @@
+﻿let userData;
+let friendData;
+let bulletinData;
+let friends = "";
+let n;
+let locateUser = "https://geekysans.github.io/socialfeed.json";
+
+getUser(locateUser);
+
+async function getUser(file) {
+  let myObject = await fetch(file);
+  let myJSON = await myObject.text();
+  userData = JSON.parse(myJSON);
+  document.getElementById('username').innerHTML = "<h1>" + userData.username + "</h1>";
+  document.getElementById('bio').innerHTML = "<h3>Bio: " + userData.bio + "</h3>";
+for (i = 0; i < userData.friends.length; i++) {
+  getFriend(userData.friends[i].userlink);
+}
+  n = userData.postbatchs;
+  getPosts(userData.posturl + n + ".json");
+}
+
+async function getFriend(file) {
+  let myObject = await fetch(file);
+  let myJSON = await myObject.text();
+  friendData = JSON.parse(myJSON);
+  getBulletin(friendData.friends[1].bulletin, friendData.username, friendData.bio);
+  }
+
+async function getBulletin(file, username, bio) {
+  let myObject = await fetch(file);
+  let myJSON = await myObject.text();
+  bulletinData = JSON.parse(myJSON);
+  displayFriend(username, bio, bulletinData.bulletinarray[0].title, bulletinData.bulletinarray[0].text);
+}
+
+function displayFriend(username, bio, bulletintitle, bulletintext) {
+  friends = friends + "<div id='friend'><h3>" + username + "</h3>" + "<h3>Bio: " + bio + "</h3>";
+  friends = friends + "<h3>Bulletins:</h3> <h3>" + bulletintitle + "</h3>" + "<h3>" + bulletintext + "</h3></div>";
+  document.getElementById('friends').innerHTML = friends;
+}
+
+function backPosts() {
+if (n > 0) {
+n = n - 1;
+getPosts(userData.posturl + n + ".json");
+}
+}
+
+function forwardPosts() {
+if (n < userData.postbatchs) {
+n = n + 1;
+getPosts(userData.posturl + n + ".json");
+}
+}
+
+async function getPosts(file) {
+  let myObject = await fetch(file);
+  let myJSON = await myObject.text();
+  let posts = JSON.parse(myJSON);
+  let string = "";
+for (i = 0; i < posts.postarray.length; i++) {
+  string = string + "<h3>" + posts.postarray[i].title + "</h3>";
+  string = string + "<p>" + posts.postarray[i].text + "</p>"
+}
+document.getElementById('posts').innerHTML = string;
+}
