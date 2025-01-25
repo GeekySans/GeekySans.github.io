8b7531763d28192a24933800c8cb16ef1bd0d89f
GeekySans <briggstaillefer10@gmail.com>
2025-01-25 12:15:32 -0800

    Update grabmenu.js

--- grabmenu.js
+++ grabmenu.js
@@ -17,7 +17,7 @@ greeting = 'Welcome'
 function loadmenu() {
 menu = [
 "<h1>GeekySans's Digital Playground" + "<h3>" + greeting + "</h3></h1>",
-"<div> <a href='/home'><button class='home'>Home</button></a> <a href='https://spacehey.com/profile?id=2706928'><button class='spacehey'>Spacehey</button></a> <a href=''><button class='forum'>Forum</button></a> <a href='/blogs'><button class='blogs'>Blogs</button></a> <a href='/games'><button class='games'>Games</button></a> <a href='/posts'><button class='posts'>Posts</button></a> </div>",
+"<div> <a href='/home'><button class='home'>Home</button></a> <a href='https://spacehey.com/profile?id=2706928'><button class='spacehey'>Spacehey</button></a> <a href=''><button class='forum'>Forum</button></a> <a href='/blogs'><button class='blogs'>Blogs</button></a> <a href='/games'><button class='games'>Games</button></a> <a href='/posts'><button class='posts'>Posts</button></a> <a href='/posts'><button class='socialfeed'>SocialFeed</button></a> </div>",
 "<div> <button onclick='back()'>Back</button> <button onclick='forward()'>Forward</button> <a href='/search'><button type='submit' class='search'><img src='spyglass.png' width='20' height='20'></button></a> </div>",
 "<hr>"
 ];
