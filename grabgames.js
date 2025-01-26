cea4678499bee0d9487762a978c7807909d6e2b0
GeekySans <briggstaillefer10@gmail.com>
2025-01-26 13:25:35 -0800

    Update grabgames.js

--- grabgames.js
+++ grabgames.js
@@ -7,7 +7,7 @@ games = [
 "<table class='stuff'>",
 "<tr>",
 "<td><h3>Cubic v2</h3><img src='Cubic-v2Logo.png' width='50%' height='50%'><p>Version 2 just came out. Grass colors have changed, you can build with more tiles, and you can choose world sizes.</p><a href='cubic-v2'><button class='TESTGAME'>Play-Game</button></a></td>",
-"<td><h3>Cubic v1</h3><img src='Cubic-v1Logo.png'><p>It's a Minecraft Indev clone that I'm working on.</p><a href='cubic-v1'><button class='TESTGAME'>Play-Game</button></a></td>",
+"<td><h3>Cubic v1</h3><img src='Cubic-v1Logo.png' width='50%' height='50%'><p>It's a Minecraft Indev clone that I'm working on.</p><a href='cubic-v1'><button class='TESTGAME'>Play-Game</button></a></td>",
  "</tr>",
  "<tr>",
   "<td><h3>TEST #2</h3><img src='arcade-TEST-2.png'><p>It's made just by some rough JS and The HTML Canvas.</p><a href='TESTGAME'><button class='TESTGAME'>Play-Game</button></a></td>",
