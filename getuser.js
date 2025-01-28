e002363c819630226cae59bf1effbbbba8311da7
GeekySans <briggstaillefer10@gmail.com>
2025-01-27 18:01:20 -0800

    Update getuser.js

--- getuser.js
+++ getuser.js
@@ -5,7 +5,7 @@ let friends = "";
 let n;
 let locateUser;
 
-const formEl = document.queryselector('.form');
+const formEl = document.querySelector('.form');
 formEl.addEventListener('submit', event => {
   event.preventDefault();
   const formData = new formData(formEl);
